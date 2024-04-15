7d3d51a00d2014144f8f13c8ef4f04bbc3568cd9
hritik kumar <hritik.7827@gmail.com>
2024-04-15 18:32:57 +0530

    added flight-middleware for recently added api

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,4 +2,5 @@ module.exports = {
   AirplaneMiddlewares: require("./airplane-middleware"),
   cityMiddleware: require("./city-middleware"),
   airportMiddleware: require("./airport-middleware"),
+  flightMiddleware: require("./flights-middleware"),
 };
--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -1,7 +1,12 @@
 const { flightController } = require("../../controllers");
+const { flightMiddleware } = require("../../middlewares");
 
 const express = require("express");
 const router = express.Router();
 
-router.post("/", flightController.createFlight);
+router.post(
+  "/",
+  flightMiddleware.validateCreateRequest,
+  flightController.createFlight
+);
 module.exports = router;
